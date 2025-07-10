527ef08a97fce30216c3d6335929861b00d0e7f7
Vanshika Srivastava <vanshikasrivastava.business@gmail.com>
2025-07-10 19:32:05 +0530

    Update user.ts

--- scripts/user.ts
+++ scripts/user.ts
@@ -1,16 +1,8 @@
 import { Sdk } from '@circles-sdk/sdk';
-import { CirclesRpc, CirclesData } from '@circles-sdk/data';
+// import { CirclesRpc, CirclesData } from '@circles-sdk/data';
 import { BrowserProviderContractRunner } from "@circles-sdk/adapter-ethers"
 
 (window as any).ethereum = web3Provider
-
-const chainConfig: ChainConfig = {
-  pathfinderUrl: 'https://pathfinder.aboutcircles.com',
-  circlesRpcUrl: 'https://rpc.aboutcircles.com',
-  v1HubAddress: '0xdbf22d4e8962db3b2f1d9ff55be728a887e47710',
-  v2HubAddress: '0x2066CDA98F98397185483aaB26A89445addD6740',
-  migrationAddress: '0x2A545B54bb456A0189EbC53ed7090BfFc4a6Af94'
-};
 
 const run = async () => {
     try {
@@ -21,8 +13,11 @@ const run = async () => {
         const address = '0xd97e35b2a5e047ed0a37edfd53daad30f28195d8'
         const sdk = new Sdk (adapter); 
         
-        const circlesRpc = new CirclesRpc("https://rpc.aboutcircles.com/");
-        const data = new CirclesData(circlesRpc, chainConfig);
+        // const circlesRpc = new CirclesRpc("https://rpc.aboutcircles.com/");
+        // const data = new CirclesData(circlesRpc);
+
+        const data = sdk.data
+
 
         // const signerAddress = await signer.getAddress()
         const avatar = await sdk.getAvatar(address)
